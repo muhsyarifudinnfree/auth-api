f40e2e51e59fa9db0bd8ab0c3fa304b7219ab425
Muhammad Syarifudin <muh.syarifudinnfree@gmail.com>
2026-04-01 16:24:51 +0700

    pass say hello world test case

--- src/Infrastructures/http/createServer.js
+++ src/Infrastructures/http/createServer.js
@@ -1,30 +1,34 @@
-import express from 'express';
-import ClientError from '../../Commons/exceptions/ClientError.js';
-import DomainErrorTranslator from '../../Commons/exceptions/DomainErrorTranslator.js';
-import users from '../../Interfaces/http/api/users/index.js';
-import authentications from '../../Interfaces/http/api/authentications/index.js';
+import express from "express";
+import ClientError from "../../Commons/exceptions/ClientError.js";
+import DomainErrorTranslator from "../../Commons/exceptions/DomainErrorTranslator.js";
+import users from "../../Interfaces/http/api/users/index.js";
+import authentications from "../../Interfaces/http/api/authentications/index.js";
 
 const createServer = async (container) => {
   const app = express();
 
   app.use(express.json());
 
-  app.use('/users', users(container));
-  app.use('/authentications', authentications(container));
+  app.use("/users", users(container));
+  app.get("/", (req, res) => {
+    res.status(200).json({ data: "Hello world!" });
+  });
+  app.use("/authentications", authentications(container));
 
   app.use((req, res) => {
     res.status(404).json({
-      status: 'fail',
-      message: 'resource not found',
+      status: "fail",
+      message: "resource not found",
     });
   });
 
-  app.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+  app.use((err, req, res, next) => {
+    // eslint-disable-line no-unused-vars
     const translatedError = DomainErrorTranslator.translate(err);
 
     if (translatedError instanceof ClientError) {
       res.status(translatedError.statusCode).json({
-        status: 'fail',
+        status: "fail",
         message: translatedError.message,
       });
       return;
@@ -33,8 +37,8 @@ const createServer = async (container) => {
     console.error(err);
 
     res.status(500).json({
-      status: 'error',
-      message: 'terjadi kegagalan pada server kami',
+      status: "error",
+      message: "terjadi kegagalan pada server kami",
     });
   });
 
